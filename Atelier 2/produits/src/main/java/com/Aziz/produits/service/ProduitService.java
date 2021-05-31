166f486df77e3094268e0ccc6146fce9ca86d6f7
Aziz-Abdellaoui <85045660+Aziz-Abdellaoui@users.noreply.github.com>
2021-05-31 10:29:53 +0100

    Add files via upload

---
+++ Atelier 2/produits/src/main/java/com/Aziz/produits/service/ProduitService.java
@@ -0,0 +1,15 @@
+package com.Aziz.produits.service;
+
+import java.util.List;
+
+import com.Aziz.produits.entities.Produit;
+
+public interface ProduitService {
+	Produit saveProduit(Produit p);
+	Produit updateProduit(Produit p);
+	void deleteProduit(Produit p);
+	void deleteProduitById(Long id);
+	Produit getProduit(Long id);
+	List<Produit> getAllProduits();
+
+}
